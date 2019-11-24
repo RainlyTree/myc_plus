042c4a750b2303a1d53f8aaed4e2b7cff0ba482f
RainlyTree <a1123845350@163.com>
2019-11-24 16:23:20 +0800

    C  与 CPP中 struct的不同

--- _text/_text/_text.cpp
+++ _text/_text/_text.cpp
@@ -100,3 +100,28 @@
 //		cout << "none";
 //	return 0;
 //}
+
+#include<iostream>
+using namespace std;
+struct Student
+{
+	void SetStudentInfo(const char* name, const char* gender, int age)
+	{
+		strcpy(_name, name);
+		strcpy(_gender, gender);
+		_age = age;
+	}
+	void PrintStudentInfo()
+	{
+		cout << _name << " " << _gender << " " << _age << endl;
+	}
+	char _name[20];
+	char _gender[3];
+	int _age;
+};
+int main()
+{
+	Student s;
+	s.SetStudentInfo("Peter", "ÄÐ", 18);
+	return 0;
+}
