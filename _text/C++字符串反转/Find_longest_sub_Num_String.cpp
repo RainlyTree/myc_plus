23f65af206a2464600c1127c2bf04bfecf67817d
RainlyTree <a1123845350@163.com>
2019-11-28 19:16:33 +0800

    更新找最长数字串的方法

--- _text/C++字符串反转/Find_longest_sub_Num_String.cpp
+++ _text/C++字符串反转/Find_longest_sub_Num_String.cpp
@@ -5,38 +5,66 @@
 #include<string>
 using namespace std;
 
-bool Gethuiwen(string::iterator& A, string::iterator& B)
-{
-	while (A < B)
-	{
-		if (*A != *B)
-			return false;
-		A++;
-		B++;
-	}
-	return true;
-}
+//方法一
+//bool Gethuiwen(string::iterator& A, string::iterator& B)
+//{
+//	while (A < B)
+//	{
+//		if (*A != *B)
+//			return false;
+//		A++;
+//		B++;
+//	}
+//	return true;
+//}
+//
+//int GetNum(string& A, string& B)
+//{
+//	int sum = 0;
+//	int num = A.size();
+//	while (num >= 0)
+//	{
+//		string tmp(A);
+//		tmp.insert(num, B);
+//		string::iterator beg = tmp.begin();
+//		string::iterator end = tmp.end() - 1;
+//		if (Gethuiwen(beg, end))
+//			sum++;
+//	}
+//}
+//
+//int main()
+//{
+//	string A;
+//	string B;
+//	getline(cin, A);
+//	getline(cin, B);
+//	int num = GetNum(A, B);
+//	cout << num;
+//	return 0;
+//}
 
-int GetNum(string& A, string& B)
-{
-	int sum = 0;
-	int num = A.size();
-	while (num >= 0)
-	{
-		string tmp(A);
-		tmp.insert(num, B);
-		if (Gethuiwen(tmp.begin(), tmp.end() - 1))
-			sum++;
-	}
-}
-
+//方法二
 int main()
 {
-	string A;
-	string B;
-	getline(cin, A);
-	getline(cin, B);
-	int num = GetNum(A, B);
-	cout << num;
+	string str, res, cur;
+	cin >> str;
+	for (int i = 0; i <= str.length(); ++i)
+	{
+		//数字+=到cur
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			cur += str[i];
+		}
+		else
+		{
+			//找出更长的字符串,则更新
+			if (res.size() < cur.size())
+				res = cur;
+			else
+				cur.clear();
+		}
+	}
+	cout << res;
 	return 0;
 }
