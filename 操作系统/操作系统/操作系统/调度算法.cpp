d744ab90e22e29b21a1d24018641b779b10ced9e
RainlyTree <a1123845350@163.com>
2019-12-02 16:52:15 +0800

    完成最小和权限算法

--- 操作系统/操作系统/操作系统/调度算法.cpp
+++ 操作系统/操作系统/操作系统/调度算法.cpp
@@ -5,29 +5,39 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<queue>
 using namespace std;
 
 struct Node
 {
 	string name = "1111111111";	//进程标识符
-	int Tarrive = 0;	//进入时间
+	float Tarrive = 0;	//进入时间
 	float Tservice = 0;	//服务时间
-	int Tsurplus = 0;	//剩余时间
-	int Tstart = 0;		//开始时间
-	int Taccomplish = 0;//完成时间
+	float Tsurplus = 0;	//剩余时间
+	float Tstart = 0;		//开始时间
+	float Taccomplish = 0;//完成时间
 	float TZz = 0;		//周转时间
 	float TDqZz = 0;		//带权周转时间
 	int prio = 0;		//优先级-->数字越大优先级越高
-	int if_finish = 0;	//进程是否完成
+	bool flag = false;	//是否进入时间片的队列
 	//int num;		//进程个数
 };
+//将剩余时间设置成服务时间
+void SetTime(int num, vector<Node*>& job)
+{
+	for (int i = 0; i < num; ++i)
+	{
+		job[i]->Tsurplus = job[i]->Tservice;
+	}
+}
 
 //按到达时间排序
 void Arrive_sort(int num, vector<Node*>& job)
 {
-	for (int i = job.size() - num; i < num; i++)
+	int tmp = job.size();
+	for (int i = tmp - num; i < tmp; i++)
 	{
-		for (int j = i + 1; j < num; j++)
+		for (int j = i + 1; j < tmp; j++)
 		{
 			if (job[i]->Tarrive > job[j]->Tarrive)
 			{
@@ -40,9 +50,10 @@ void Arrive_sort(int num, vector<Node*>& job)
 //按服务时间排序
 void Service_sort(int num, vector<Node*>& job)
 {
-	for (int i = job.size() - num; i < num; i++)
+	int tmp = job.size();
+	for (int i = tmp - num; i < tmp; i++)
 	{
-		for (int j = i + 1; j < num; j++)
+		for (int j = i + 1; j < tmp; j++)
 		{
 			if (job[i]->Tservice > job[j]->Tservice)
 			{
@@ -55,9 +66,10 @@ void Service_sort(int num, vector<Node*>& job)
 //按优先级排序
 void Priority_sort(int num, vector<Node*>& job)//按优先级减小排序
 {
-	for (int i = 1; i < num; i++)
+	int tmp = job.size();
+	for (int i = tmp - num; i < tmp; i++)
 	{
-		for (int j = i + 1; j < num; j++)
+		for (int j = i + 1; j < tmp; j++)
 		{
 			if (job[i]->prio < job[j]->prio)
 			{
@@ -127,10 +139,8 @@ void fcfs(int num, vector<Node*>& job)//先来先服务
 //短作业优先
 void sjf(int num, vector<Node*> job)
 {
-	//当前的结束时间
-	float tmpnum = num;
 	//按到达时间排序
-	Arrive_sort(tmpnum, job);
+	Arrive_sort(num, job);
 	//第一个要进行的任务
 	for (int i = 1; i < num; ++i)
 	{
@@ -140,7 +150,7 @@ void sjf(int num, vector<Node*> job)
 			swap(job[0], job[1]);
 		}
 	}
-	--tmpnum;
+	job[0]->Tstart = job[0]->Tarrive;
 	//完成时间 = 开始 + 服务时间
 	job[0]->Taccomplish = job[0]->Tstart + job[0]->Tservice;
 	//周转时间 == 完成时间-到达时间
@@ -150,127 +160,188 @@ void sjf(int num, vector<Node*> job)
 
 	for (int i = 1; i < num; i++)
 	{
+		//寻找第i个进入的进程
 		//对剩下的进行服务排序
-		Service_sort(tmpnum, job);
+		Service_sort(num - i, job);
 		//记录当前进行的位置
 		int tmp = i;
 		//判断当前的最小服务时间是否在已经进入
 		for (int j = i; j < num; ++j)
 		{
-			//如果在已经进入
-			if (job[j]->Tstart < job[i - 1]->Taccomplish)
+			//如果服务时间最小的到达时间 < 前一个完成的完成时间
+			if (job[j]->Tarrive < job[i - 1]->Taccomplish)
 			{
+				swap(job[j], job[tmp]);
 				break;
 			}
-			//还没有进入
-			else
-			{
-
-			}
 		}
-		job[i]->Tstart = job[i - 1]->Taccomplish;//上一个作业结束时间
-		if (job[i]->Tstart < job[i]->Tarrive)//该作业的开始时间小于到达时间
-		{
-			job[i]->Tstart = job[i]->Tarrive;
-		}
+		if (job[i]->Tarrive < job[i - 1]->Taccomplish)
+			job[i]->Tstart = job[i - 1]->Taccomplish;//上一个作业结束时间
 		else
 		{
-			job[i]->Tstart = job[i - 1]->Taccomplish;
+			//所有的都比他完成之后都要晚  再次进行时间排序
+			Arrive_sort(num, job);
+			for (int k = tmp + 1; k < num; ++k)
+			{
+				//如果找到时间与第一个相同的时间但服务时间比他小 交换
+				if (job[tmp]->Tarrive == job[k]->Tarrive && job[tmp]->Tservice > job[k]->Tservice)
+				{
+					swap(job[tmp], job[k]);
+				}
+			}
+			job[i]->Tstart = job[i]->Tarrive;
+
 		}
+
 		job[i]->Taccomplish = job[i]->Tstart + job[i]->Tservice;
 		job[i]->TZz = job[i]->Taccomplish - job[i]->Tarrive;
 		job[i]->TDqZz = job[i]->TZz / job[i]->Tservice;
 	}
 }
 
+//高优先级优先
+void High_level(int num, vector<Node*> job)
+{
+	//按到达时间排序
+	Arrive_sort(num, job);
+	//第一个要进行的任务
+	for (int i = 1; i < num; ++i)
+	{
+		//如果找到时间与第一个相同的时间但权限比它大的 交换
+		if (job[0]->Tarrive == job[i]->Tarrive && job[0]->prio < job[i]->prio)
+		{
+			swap(job[0], job[1]);
+		}
+	}
+	job[0]->Tstart = job[0]->Tarrive;
+	//完成时间 = 开始 + 服务时间
+	job[0]->Taccomplish = job[0]->Tstart + job[0]->Tservice;
+	//周转时间 == 完成时间-到达时间
+	job[0]->TZz = job[0]->Taccomplish - job[0]->Tarrive;
+	//带权周转时间 == 周转时间/服务时间
+	job[0]->TDqZz = job[0]->TZz / job[0]->Tservice;
+
+	for (int i = 1; i < num; i++)
+	{
+		//寻找第i个进入的进程
+		//对剩下的进行优先级排序
+		Priority_sort(num - i, job);
+		//记录当前进行的位置
+		int tmp = i;
+		//判断当前的最大权限是否在已经进入
+		for (int j = i; j < num; ++j)
+		{
+			//如果权限最大的到达时间 < 前一个完成的完成时间
+			if (job[j]->Tarrive < job[i - 1]->Taccomplish)
+			{
+				swap(job[j], job[tmp]);
+				break;
+			}
+		}
+		if (job[i]->Tarrive < job[i - 1]->Taccomplish)
+			job[i]->Tstart = job[i - 1]->Taccomplish;//上一个作业结束时间
+		else
+		{
+			//所有的都比他完成之后都要晚  再次进行时间排序
+			Arrive_sort(num, job);
+			for (int k = tmp + 1; k < num; ++k)
+			{
+				//如果找到时间与第一个相同的时间但权限比它大的 交换
+				if (job[tmp]->Tarrive == job[k]->Tarrive && job[tmp]->Tservice < job[k]->Tservice)
+				{
+					swap(job[tmp], job[k]);
+				}
+			}
+			job[i]->Tstart = job[i]->Tarrive;
+
+		}
+
+		job[i]->Taccomplish = job[i]->Tstart + job[i]->Tservice;
+		job[i]->TZz = job[i]->Taccomplish - job[i]->Tarrive;
+		job[i]->TDqZz = job[i]->TZz / job[i]->Tservice;
+	}
+
+}
+
 //时间片轮转算法
 void RR(int num, vector<Node*> job)//RR算法
 {
+	//设置剩余时间
+	SetTime(num, job);
+	//获取时间片
 	int q;
 	cout << "请输入时间片长度：" << endl;
 	cin >> q;
-	int flag = 1;//标志队列中是否还有进程
-	int finish_pro = 0;//完成的进程数
-	cout << "进程名称\t" << "开始时间\t" << "运行时间\t" << "剩余服务时间\t" << "结束时间\t" << endl;
-	int time;//记录当前时刻时间
-	int c = 0;
-	while (finish_pro < num)
+	//将等待序列放入队列中
+	queue<Node*> job_que;
+	//当前的时间
+	int time = 0;
+	//进行时间排序
+	Arrive_sort(num, job);
+	//开始时间为第一个作业的到达时间
+	time = job[0]->Tarrive;
+	job[0]->Tstart = time;
+	job_que.push(job[0]);
+	job[0]->flag = true;
+	while (!job_que.empty())
 	{
-		flag = 0;//就绪队列里没进程
-		for (int i = c; i < num; i++)
+		//获取当前需要进行的作业
+		Node* tmp_job = job_que.front();
+		job_que.pop();
+		//如果开始时间没有设定，将当前时间设为开始时间
+		if ((tmp_job->Tstart == 0 && (tmp_job != job[0])))
 		{
-			//到达排序
-			Arrive_sort(num, job);
-			//把服务时间给剩余时间
-			job[i]->Tsurplus = job[i]->Tservice;
-			//当前的开始时间 = 上一个作业结束时间
-			job[i]->Tstart = job[i - 1]->Taccomplish;
-			//该作业的开始时间小于到达时间
-			if (job[i]->Tstart < job[i]->Tarrive)
-			{
-				job[i]->Tstart = job[i]->Tarrive;
-			}
-			else
-			{
-				job[i]->Tstart = job[i - 1]->Taccomplish;
-			}
-			time = job[i]->Tstart;
-			if (job[i]->if_finish == 1) 
-				continue;//该进程已完成
-			else
-			{
-				if (job[i]->Tsurplus <= q && time >= job[i]->Tarrive)//未完成且少于一个时间片
-				{
-					flag = 1;
-					time = time + job[i]->Tsurplus;
-					job[i]->if_finish = 1;//该进程完成
-					job[i]->Taccomplish = time;
-					cout << job[i]->name << "\t\t" << job[i]->Taccomplish - job[i]->Tsurplus << "\t\t" << job[i]->Tsurplus << "\t\t" << 0 << "\t\t" << job[i]->Taccomplish << endl;
-					job[i]->Tsurplus = 0;
-				}
-				else if (job[i]->Tsurplus > q && time >= job[i]->Tarrive)
-				{
-					flag = 1;
-					time = time + q;
-					job[i]->Tsurplus -= q;
-					job[i]->Taccomplish = time;
-					cout << job[i]->name << "\t\t" << time - q << "\t\t" << q << "\t\t" << job[i]->Tsurplus << "\t\t" << job[i]->Taccomplish << endl;
-					job[num]->name = job[i]->name;
-					job[num]->Tarrive = time;
-					job[num]->Tservice = job[i]->Tsurplus;
-					num++;
-				}
-				if (job[i]->if_finish == 1) finish_pro++;//一个进程完成加一
-			}
-			c++;
-		}break;
-		if (flag == 0 && finish_pro < num)//没执行完且没进入就绪队列
+			tmp_job->Tstart = time;
+		}
+		//判断当前所需要的时间是否比当前的时间片小
+		if (tmp_job->Tsurplus <= q)
 		{
-			for (int i = 0; i < num; i++)
+			//时间加上当前剩余时间
+			time += tmp_job->Tsurplus;
+			//设置完成时间
+			tmp_job->Taccomplish = time;
+		}
+		//还需要不止一个时间片
+		else
+		{
+			//时间 直接加上时间片
+			tmp_job->Tsurplus -= q;
+			time += q;
+		}
+		for (int i = 0; i < num; ++i)
+		{
+			//不是当前进程   未完成	 已经到达   没有进入队列
+			if (job[i] != tmp_job && job[i]->Taccomplish == 0 && job[i]->Tarrive <= time && job[i]->flag == false)
 			{
-				if (job[i]->if_finish == 0)
-				{
-					time = job[i]->Tarrive;
-					break;
-				}
+				job_que.push(job[i]);
+				job[i]->flag = true;
 			}
 		}
+		//没有完成时间
+		if(tmp_job->Taccomplish == 0)
+			job_que.push(tmp_job);
+	}
+	for (int i = 0; i < num; ++i)
+	{
+		job[i]->Taccomplish = job[i]->Tstart + job[i]->Tservice;
+		job[i]->TZz = job[i]->Taccomplish - job[i]->Tarrive;
+		job[i]->TDqZz = job[i]->TZz / job[i]->Tservice;
 	}
 }
 
 //输出函数
 void print(int num, vector<Node*>& job)
 {
-	cout << "进程名" << "\t" << "到达时间" << "\t" << "服务时间" << "\t" << "完成时间" 
-		<< "\t" << "周转时间" << "\t" << "带权周转" << endl;
+	cout << "进程名" << "\t" << "到达时间" << "\t" << "服务时间" << "\t" << "开始时间" <<
+		"\t" << "完成时间" << "\t" << "周转时间" << "\t" << "带权周转" << endl;
 
 	for (int i = 0; i < num; i++)
 	{
-		cout << job[i]->name << "\t" << job[i]->Tarrive << "\t\t" << job[i]->Tservice << "\t\t" << job[i]->Taccomplish 
+		cout << job[i]->name << "\t" << job[i]->Tarrive << "\t\t" << job[i]->Tservice 
+			<< "\t\t"  << job[i]->Tstart << "\t\t" << job[i]->Taccomplish
 			<< "\t\t" << job[i]->TZz << "\t\t"  << job[i]->TDqZz << endl;
 	}
 }
-
 
 
 void display(int num,vector<Node*> job)
@@ -298,9 +369,11 @@ void display(int num,vector<Node*> job)
 			break;
 		case 3:
 			RR(num, job);
+			print(num, job);
 			break;
 		case 4:
-			/*Priority_sort(num);*/
+			High_level(num, job);
+			print(num, job);
 			break;
 		case 5:
 			exit;
