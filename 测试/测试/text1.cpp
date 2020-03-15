9f99b08456f82d7b65e3911558246f4c0505b6b5
RainlyTree <a1123845350@163.com>
2020-03-15 21:12:00 +0800

    leetcode

--- 测试/测试/text1.cpp
+++ 测试/测试/text1.cpp
@@ -194,14 +194,78 @@ private:
 };
 
 
+int lengthOfLIS(vector<int>& nums) {
+	if (nums.empty() == 1)
+		return 0;
+	vector<int> fin;
+	fin.push_back(nums[0]);
+	for (int i = 1; i < nums.size(); ++i)
+	{
+		if (nums[i] > fin[fin.size() - 1])
+		{
+			fin.push_back(nums[i]);
+		}
+		else
+		{
+			int fir = 0, sec = fin.size() - 1, len = 0;
+			while (fir <= sec)
+			{
+				int in_ = (fir + sec) >> 1;
+				if (fin[in_] > nums[i])
+				{
+					sec = in_ - 1;
+				}
+				else
+				{
+					len = in_;
+					fir = in_ + 1;
+				}
+			}
+			if (fin[len] == nums[i])
+				;
+			else
+				fin[len + 1] = nums[i];
+		}
+	}
+	return fin.size();
+
+}
+
+int dfs(vector<vector<int>>& grid, int cur_i, int cur_j)
+{
+	if (cur_i < 0 || cur_j < 0 || cur_i == grid.size() || cur_j == grid[0].size()
+		|| grid[cur_i][cur_j] == 0)
+		return 0;
+	grid[cur_i][cur_j] == 0;
+	int di[4] = { 0, 0, 1, -1 };
+	int dj[4] = { 1, -1, 0, 0 };
+	int ans = 1;
+	for (int k = 0; k < 4; k++)
+	{
+		int n_i = cur_i + di[k];
+		int n_j = cur_j + dj[k];
+		ans += dfs(grid, n_i, n_j);
+	}
+	return ans;
+}
+	int maxAreaOfIsland(vector<vector<int>>& grid) {
+		int row = grid[0].size();
+		int col = grid.size();
+		int len = 0;
+		for (int i = 0; i < col; ++i)
+		{
+			for (int j = 0; j < row; ++j)
+			{
+				len = max(len, dfs(grid, i, j));
+			}
+		}
+		return len;
+	}
+
+
 int main()
 {
-	Base b;
-
-	cout << sizeof(Base) << endl;
-	cout << sizeof(Derive) << endl;
-	
-	Derive d;
-
+	vector<vector<int>> fin = { {1,1,0,0,0},{1,1,0,0,0},{0,0,0,1,1},{0,0,0,1,1} };
+	maxAreaOfIsland(fin);
 	return 0;
 }
