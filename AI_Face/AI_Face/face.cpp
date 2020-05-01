adb057160118b53532bbf516f263de5e205d549c
RainlyTree <a1123845350@163.com>
2020-05-01 16:56:06 +0800

    方差 协方差

---
+++ AI_Face/AI_Face/face.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	//获取图片
+	Mat image = imread("D:/My_file/myc_plus/AI_Face/image/test3.png");
+	if (image.empty())
+	{
+		cout << "not load image..." << endl;
+		return -1;
+	}
+	imshow("input image", image);
+
+	//均值 方差
+	Mat means, stddev;
+	meanStdDev(image, means, stddev);
+	cout << "means rows:" << means.rows << " "
+		<< "means cols:" << means.cols << endl;
+	cout << "stddev rows:" << stddev.rows << " "
+		<< "stddev cols:" << stddev.cols << endl;
+	for (int row = 0; row < means.rows; row++)
+	{
+		printf("mean %d = %.3f\n", row, means.at<double>(row));
+		printf("stddev %d = %.3f\n", row, stddev.at<double>(row));
+	}
+
+	Mat samples = (Mat_<double>(5, 3) 
+		<< 90, 60, 90, 90, 90, 30, 60, 60, 60, 60, 60, 90, 30, 30, 30);
+	Mat cov, mu;
+	calcCovarMatrix(samples, cov, mu, CV_COVAR_NORMAL | CV_COVAR_ROWS);
+
+	cout << "==================================" << endl;
+	cout << "cov: " << endl;
+	cout << cov << endl;
+
+	cout << "means: " << endl;
+	cout << mu << endl;
+
+	waitKey(0);
+	return 0;
+}
--- 测试/测试/text1.cpp
+++ 测试/测试/text1.cpp
@@ -1,94 +1,31 @@
-// write your code here cpp
-#include<iostream>
-#include<vector>
-using namespace std;
+int main() {
+	int num;
+	while (cin >> num) {
+		string str;
+		string word, s;
+		int index;
+		vector<string> vs;
+		for (int i = 0; i < num; ++i) {
+			cin >> str;
+			vs.push_back(str);
+		}
+		sort(vs.begin(), vs.end());  // 因为是字典，一定要排序！！
+		cin >> word;
+		cin >> index;
+		int counts = 0;
 
-void test(vector<vector<char>>& fin)
-{
-	for (int i = 0; i < 20; ++i)
-	{
-		for (int j = 0; j < 20; ++j)
-		{
-			if (fin[i][j] == '.')
-				continue;
-			else
-			{
-				int tmpi = i;
-				int tmpj = j;
-				int num = 1;
-				//向右
-				while (tmpj + 1 < 20 && fin[tmpi][tmpj] == fin[tmpi][tmpj + 1])
-				{
-					++tmpj;
-					++num;
-					if (num == 5)
-					{
-						cout << "Yes" << endl;
-						return;
-					}
-				}
-				num == 0;
-				//向下
-				while ((tmpi + 1) < 20 && fin[tmpi][tmpj] == fin[tmpi + 1][tmpj])
-				{
-					++tmpi;
-					++num;
-					if (num == 5)
-					{
-						cout << "Yes" << endl;
-						return;
-					}
-				}
-				num == 0;
-				//右下
-				while (tmpi + 1 < 20 && tmpj + 1 < 20 && fin[tmpi][tmpj] == fin[tmpi + 1][tmpj + 1])
-				{
-					++tmpj;
-					++tmpi;
-					++num;
-					if (num == 5)
-					{
-						cout << "Yes" << endl;
-						return;
-					}
-				}
-				num == 0;
-				//左下
-				while (tmpi + 1 < 20 && tmpj - 1 >= 0 && fin[tmpi][tmpj] == fin[tmpi + 1][tmpj - 1])
-				{
-					--tmpj;
-					++tmpi;
-					++num;
-					if (num == 5)
-					{
-						cout << "Yes" << endl;
-						return;
-					}
-				}
+		for (int i = 0; i < num; ++i) {
+			if (isBrother(word, vs[i])) {
+				counts++;
+				if (counts == index)
+					s = vs[i];
 			}
 		}
-	}
+		if (!vs.empty())
+			cout << counts << endl;
+		if (counts >= index)
+			cout << s << endl;
 
-	cout << "No" << endl;
-}
-
-int main()
-{
-	vector<vector<char>> fin(20, vector<char>(20));
-	char c;
-	while ((c = getchar()) != EOF)
-	{
-		ungetc(c, stdin);
-		for (int i = 0; i < 20; ++i)
-		{
-			for (int j = 0; j < 20; ++j)
-			{
-				c = getchar();
-				fin[i][j] = c;
-			}
-			getchar();
-		}
-		test(fin);
 	}
 	return 0;
 }
