d7ed36b830fed2dbc090f0e731f8134a901adf78
RainlyTree <a1123845350@163.com>
2020-09-14 21:28:00 +0800

    笔试三次

--- 复习/复习/test.cpp
+++ 复习/复习/test.cpp
@@ -1,30 +1,57 @@
-#include<iostream>
 #include<vector>
-#include<algorithm>
+#include<iostream>
 using namespace std;
+
+void  _GetSum(vector<vector<int>>& fin, vector<vector<int>>& tmp, int row, int col, int i, int j)
+{
+	if (fin[i][j] == 1 && tmp[i][j] == 0)
+	{
+		tmp[i][j] = 1;
+		if (i - 1 >= 0)
+			_GetSum(fin, tmp, row, col, i - 1, j);
+		if (i + 1 < row)
+			_GetSum(fin, tmp, row, col, i + 1, j);
+		if (j - 1 >= 0)
+			_GetSum(fin, tmp, row, col, i, j - 1);
+		if (j + 1 < col)
+			_GetSum(fin, tmp, row, col, i, j + 1);
+	}
+	return;
+}
+
+int GetSum(vector<vector<int>>& fin, int row, int col, int count)
+{
+	int Sum = 0;
+	vector<vector<int>> tmp(row, vector<int>(col, 0));
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			if (fin[i][j] == 1 && tmp[i][j] == 0)
+			{
+				_GetSum(fin, tmp, row, col, i, j);
+				++Sum;
+			}
+		}
+	}
+	return Sum;
+}
+
 int main()
 {
-	int num = 0;
-	//while (cin >> num)
+	int row, col;
+	cin >> row >> col;
+	vector<vector<int>> fin(row, vector<int>(col, 0));
+	int count = 0;
+	for (int i = 0; i < row; ++i)
 	{
-		vector<int> arr(1000000 , 0);
-		arr[1000000 - 1] = 1000000
-		if (num < 2)
+		for (int j = 0; j < col; ++j)
 		{
-			cout << 0 << endl;
-			//continue;
+			cin >> fin[i][j];
+			if (fin[i][j] == 1)
+				++count;
 		}
-		for (int i = 0; i < num; ++i)
-		{
-			cin >> arr[i];
-		}
-		sort(arr.begin(), arr.end());
-		int sum = 0;
-		for (int i = 0; i < num - 1; ++i)
-		{
-			sum = sum * 2 + arr[i + 1] - arr[i];
-		}
-		cout << sum << endl;
 	}
+	int sum = GetSum(fin, row, col, count);
 	return 0;
 }
